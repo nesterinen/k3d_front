b69f229b18ed517b4d3165257cf5f2f2a60930d2
Alekei <aleksei.nesterinen@outlook.com>
2025-01-31 10:06:17 +0200

    changed api to wcs service

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,9 +1,22 @@
 import axios from 'axios'
 import { backEndUrl } from '../utils/config'
 
+// /api/v1/korkeusmalli2m uses ogc api process which is really slow...
+/*
 const getTifFile = async (latitude: number, longitude: number, size=1000): Promise<ArrayBuffer> => {
     return await axios.post(
         backEndUrl + '/api/v1/korkeusmalli2m',
+        { latitude, longitude, size },
+        { responseType: 'arraybuffer' }
+    ).then(result => {
+        return result.data
+    })
+}
+*/
+
+export const getTifFile = async (latitude: number, longitude: number, size=1000): Promise<ArrayBuffer> => {
+    return await axios.post(
+        backEndUrl + '/wcs/v1/korkeusmalli_2m',
         { latitude, longitude, size },
         { responseType: 'arraybuffer' }
     ).then(result => {
