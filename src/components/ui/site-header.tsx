f8554b718dfc2e42f262148eee7bb3f6f21ea28c
Aleksei Nesterinen <aleksei.nesterinen@outlook.com>
2025-02-20 16:35:52 +0200

    custom svg logo and added it to page header

--- src/components/ui/site-header.tsx
+++ src/components/ui/site-header.tsx
@@ -1,10 +1,16 @@
 import { ModeSwitch } from './mode-toggle'
+import myLogo from '/mySvg.svg'
 
 
 export function SiteHeader() {
     return (
         <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
             <div className="flex h-14 items-center px-4">
+
+                <div className='mr-1 md:flex'>
+                    <img src={myLogo} className='h-5 w-5 flex-no-shrink fill-current' alt='mylogo'/>
+                </div>
+
                 <div className='mr-4 md:flex'>
                     <h1 className='text-xl font-semibold'>Kartta3D</h1>
                 </div>
